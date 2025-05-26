3260b9f0cea59872b765c012586593ce6cbfd532
Sanad Sonone <155049358+SanadSonone@users.noreply.github.com>
2025-05-26 14:16:23 +0000

    react router

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Route, RouterProvider,createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Layout from './Layout'
+import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
-import Contact from './components/Contact/Contact'
+import Contact from './components/Contact/Contact.jsx'
 // const router = createBrowserRouter([
 //   {
 //     path:'',
